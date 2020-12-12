559f662244158f70a52e91762e0a80756dc19439
biswa7064 <biswaranjan16856@gmail.com>
2020-12-12 21:50:28 +0530

    added navigation bar and route and pages component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom'
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Start from './pages/Start';
 
 function App() {
+  
   return (
+    <div>
+      <Navs/>
     <Switch>
       <Route exact path = "/">
-        This is home page
+        <Home />
       </Route>
       <Route  path = "/start">
-        This is Start page
+        <Start />
       </Route>
       <Route>404 Not Found</Route>
     </Switch>
+    </div>
   );
 }
 
