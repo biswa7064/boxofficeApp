c448a8e81c6418c6f9844b5c3199836076265c0b
biswa7064 <biswaranjan16856@gmail.com>
2020-12-13 18:57:41 +0530

    displaying api result and custom render function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,20 +4,21 @@
 /* eslint-disable no-unused-vars */
 import React,{useState} from 'react'
 import MainpageLayout from '../components/MainpageLayout'
+import {apiGet} from '../misc/config'
 
 const Home = () => {
     const [input, setInput ] = useState('')
+    const [results, setResults] = useState(null)
 
     const onInputChange = (event)=>{
         setInput(event.target.value);
     }
 
     const onSearch = ()=>{
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-        .then(raw=>raw.json())
+       apiGet(`/search/shows?q=${input}`)        
         .then((result)=>{
-            console.log(result)
-        })
+            setResults(result)           
+        });
     }
 
     const onKeyDown = (ev)=>{
@@ -26,10 +27,27 @@ const Home = () => {
         }
     }
 
+    const renderResults = () => {
+        if(results && results.length === 0){
+            return <div>No output</div>
+        }
+        if(results && results.length > 0){
+        return (
+        <div>
+            {results.map((item)=>(
+            <div key = {item.show.id}>{item.show.name}</div>
+            ))}
+        </div>
+        );
+        }
+        return null;
+    };
+
     return (
         <MainpageLayout>
          <input type = "text" onChange = {onInputChange} onKeyDown = {onKeyDown} value = {input} /> 
-         <button type ="button" onClick = {onSearch} >Search</button> 
+         <button type ="button" onClick = {onSearch} >Search</button>
+         {renderResults()} 
         </MainpageLayout>
     )
 }
