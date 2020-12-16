0a3c24a22edcceb14cf07036190fe3c7c3bd498a
biswa7064 <biswaranjan16856@gmail.com>
2020-12-17 02:29:47 +0530

    introduction of reducer using useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,34 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect,useState } from 'react'
+import React, { useEffect,useReducer } from 'react'
 import {useParams} from 'react-router-dom'
 import { apiGet } from '../misc/config';
 
+
+
+const initialState = {
+    show:null,
+    isLoading:true,
+    error:null
+}
+
+const reducer = (prevState, action)=>{
+    switch(action.type){
+        case 'FETCH_SUCCESS':{
+            return{isLoading:false,error:null,show:action.show }
+        }
+
+        case 'FETCH_FAILED':{
+            return{...prevState,isLoading:false,error:action.error }
+        }
+
+        default: return prevState
+    }
+}
+
 const Show = () => {
     const {id} = useParams();
-    const[show,setShow] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
+
+    const [{show,isLoading,error},dispatch] = useReducer(reducer,initialState)
 
     useEffect(()=>{
 
@@ -16,15 +37,15 @@ const Show = () => {
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
         .then(results=>{
             if(isMounted){
-            setShow(results);
-            setIsLoading(false)
+            
+                dispatch({type: 'FETCH_SUCCESS', show:results })
+            
             }
             
         })
         .catch(err=>{
             if(isMounted){
-            setError(err.message);
-            setIsLoading(false);
+                dispatch({type: 'FETCH_FAILED', error:err.message })
             }
         })
 
@@ -34,13 +55,13 @@ const Show = () => {
     },[id])
 
 
-    if(isLoading){
-        return <div>Data being loaded</div>;
-    }
+    // if(isLoading){
+    //     return <div>Data being loaded</div>;
+    // }
 
-    if(error){
-        return <div>Error occured:{error}</div>;
-    }
+    // if(error){
+    //     return <div>Error occured:{error}</div>;
+    // }
 
     
     return (
