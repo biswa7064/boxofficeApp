3ebb5b91d996188dc1098c5f130b1457f23f76a0
biswa7064 <biswaranjan16856@gmail.com>
2020-12-17 22:12:40 +0530

    using custom hooks styled the starred shows

--- src/components/show/Showgrid.js
+++ src/components/show/Showgrid.js
@@ -4,22 +4,44 @@ import Showcard from './Showcard'
 import {FlexGrid}  from '../styled'
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import { useShows } from '../../misc/custom-hooks'
 
 
 
 const Showgrid = ({data}) => {
+
+const [starredShows, dispatchStarred] = useShows();
+
     return (
         <FlexGrid>
 
          {        
-         data.map(({show})=> (
-         <Showcard key = {show.id} 
+         data.map(({show})=> {
+
+        const isStarred = starredShows.includes(show.id);
+
+        const onStarClick = ()=>{
+
+            if(isStarred){
+                dispatchStarred({type:'REMOVE', showId:show.id})
+            }else{
+                dispatchStarred({type:'ADD', showId:show.id})
+            }
+
+        }
+
+        return(
+         <Showcard 
+         key = {show.id} 
          id={show.id} 
          name = {show.name} 
          image = {show.image?show.image.medium:IMAGE_NOT_FOUND}
          summary = {show.summary}
-         />))
-         }
+         onStarClick = {onStarClick}
+         isStarred = {isStarred}
+         />
+        );         
+        })}
         </FlexGrid>
     )
 }
